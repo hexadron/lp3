1176148a3d0ee2e04b3de00f2a22424c50f85a18
Carlos Huamani <carlos.hs.92@gmail.com>
2012-06-01 02:01:46 -0500

    facebook T_T

--- maquetacion/js/funciones.js
+++ maquetacion/js/funciones.js
@@ -6,9 +6,9 @@ $(function(){
 				
 	$.getJSON('https://api.twitter.com/1/statuses/user_timeline/carloshs92.json?callback=?',
 		function(data){
-			for(var i=0; i<2; i++)
+			for(var i=0; i<3; i++)
 			{
-				$("#twitter ul").append("<li><img src='img/logo.png' alt='face"+i+"' width='50'/><span class='textTwitt'>"+data[i].text+"</span></li>");
+				$("#twitter ul").append("<li><img src='img/twitt.png' alt='face"+i+"' width='50'/><span class='textTwitt'>"+data[i].text+"</span></li>");
 			}
 		}
 	);
